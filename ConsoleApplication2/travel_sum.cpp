59b2c6807c8aacba6d5cb0896a8c521c9d8291f1
googaw <1482509119@qq.com>
2025-02-17 11:06:38 +0800

    2-17

--- ConsoleApplication2/travel_sum.cpp
+++ ConsoleApplication2/travel_sum.cpp
@@ -50,20 +50,84 @@ const int hour_to_min = 60;
 //}
 
 //编写一个程序可以输入最多10个高尔夫球的成绩，用户允许提前结束输入，并存储到一个数组中，写三个函数算平均值，输入，输出
-using namespace std;
+//using namespace std;
+//
+//int main()
+//{
+//	
+//}
+//
+//int* input()
+//{
+//	int count = 0;
+//	int golf[10];
+//	while (count<=10 and cin>>golf[count])
+//	{
+//		
+//		count++;
+//	}
+//}
+//
 
-int main()
+////使用函数的递归写一个阶乘
+//using namespace std;
+//int di_gui(int);
+//void main()
+//{
+//	
+//	int m;
+//	cin >> m;
+//	cout<< di_gui(m);
+//}
+//
+////递归函数
+//int di_gui(int n)
+//{	
+//	if (n > 1)
+//	{	
+//		return n*di_gui(n-1);
+//	}
+//
+//	else if(n == 0 or n == 1)
+//	{
+//		return 1;
+//	}
+//}
+
+using namespace std;
+int Fill_array(double*, int);
+void show_array(double*, int);
+double* Reverse_array(double*, int);
+
+void main()
+{
+	double arr_1[60] = {0};
+	cout << Fill_array(arr_1, 60);
+	
+}
+//统计输入个数
+int Fill_array(double*num, int n)
+{
+	int i = 0;
+	cout << "请输入double值";
+	while (cin >> num[i] and i++<n)
+	{
+	}
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(1000, '\n');
+	}
+	return i;
+}
+
+
+void show_array(double*, int)
 {
 	
 }
 
-int* input()
+double* Reverse_array(double*num, int)
 {
-	int count = 0;
-	int golf[10];
-	while (count<=10 and cin>>golf[count])
-	{
-		
-		count++;
-	}
+	return num;
 }
